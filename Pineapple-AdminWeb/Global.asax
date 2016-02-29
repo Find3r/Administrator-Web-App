3ea3d408d0fd5587b4343b26ba85f00b1ee25388
Carlos Castro Brenes <ccasbre27@hotmail.com>
2016-02-28 18:34:41 -0600

    init

---
+++ Pineapple-AdminWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Pineapple_AdminWeb.MvcApplication" Language="C#" %>
---
+++ Pineapple-AdminWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Pineapple_AdminWeb.Startup))]
+namespace Pineapple_AdminWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
